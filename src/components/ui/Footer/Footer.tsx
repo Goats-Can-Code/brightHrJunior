de0b13ab48cbbd33b49e6c6c01a0f0a6e298ee51
djmahony <danmahony85@gmail.com>
2024-12-06 15:18:46 +0000

    add header and footer components

---
+++ src/components/ui/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+import styles from './Footer.module.scss';
+
+export default function Footer() {
+    return (
+        <footer><p className={styles.copy}>&copy; Dan Mahony 2024</p></footer>
+    )
+}
---
+++ src/components/ui/Header/Header.tsx
@@ -0,0 +1,10 @@
+import logo from '../../../assets/images/logo-white.svg';
+import styles from './Header.module.scss';
+
+export default function Header() {
+    return (
+        <header>
+            <img src={logo as unknown as string} className={styles.logo} />
+        </header>
+    )
+}
