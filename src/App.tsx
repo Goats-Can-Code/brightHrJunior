84d83edf7485c41730aed8d4311c7b2f333a8ebe
djmahony <danmahony85@gmail.com>
2024-12-06 15:16:46 +0000

    Add the header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import './components/FileViewer/FileViewer';
 import FileViewer from './components/FileViewer/FileViewer';
+import Header from './components/ui/Header/Header';
+import Footer from './components/ui/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
+        <Header />
         <FileViewer />
+        <Footer />
     </div>
   );
 }
